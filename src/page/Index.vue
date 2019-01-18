21f6fde0009c6fdd11e97001f6b4aa304fb17ab2
Chase Ries <hello@chaseries.com>
2019-01-18 14:30:31 -0500

    Basic skeleton to start out with

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    Home page
-    <div v-for="postNo in [1,2,3,4,5]" :key="postNo">
-      <router-link :to="'/post/' + postNo">Post {{ postNo }}</router-link>
-    </div>
+  <div class="container">
+    <canvas id="canvas" resize></canvas>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: "page-index"
+  name: "page-index",
+  mounted () {
+    if (process.env.VUE_ENV === "client") {
+      const paper = require("paper");
+      const canvas = document.getElementById("canvas");
+    }
+  }
 };
 </script>
+
+<style lang="sass">
+
+.container
+  position: relative
+  height: 100vh
+  width: 100vh
+
+canvas[resize]
+  position: absolute
+  display: block
+  top: 0
+  left: 0
+  width: 100%
+  height: 100%
+
+</style>
